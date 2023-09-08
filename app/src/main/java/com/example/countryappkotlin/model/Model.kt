4ff670fcc018501cfbcfa742bf912701981b4074
burak <burakduvakli@hotmail.com>
2023-09-08 16:27:28 +0300

    Adapter & Model added

---
+++ app/src/main/java/com/example/countryappkotlin/model/Model.kt
@@ -0,0 +1,3 @@
+package com.example.countryappkotlin.model
+
+data class Model(val countryName : String?, val countryCapital : String?, val countryRegion : String?, val countryCurrency : String?, val countryLanguage : String?, val imageUrl : String?)
